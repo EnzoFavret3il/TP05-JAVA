d7abfa73d3858c1f797c2ddd8d86fb52726cfb90
EnzoFavret3il <favrete@3il.fr>
2023-12-11 16:01:18 +0100

    +

--- EntierNaturel.java
+++ EntierNaturel.java
@@ -1,4 +1,16 @@
 
 public class EntierNaturel {
+	private int entier;
 
+	public EntierNaturel(int entier) throws @MauvaiseConstanteException(){
+		super();
+		
+		if (entier<0)
+			throw new MauvaiseConstanteException();
+		this.entier = entier;
+		
+	}
+	
+	
+	
 }
