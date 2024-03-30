70cd5ebbcf366b80ce3f8b6e9bfd7aee77c61327
EnzoFavret3il <favrete@3il.fr>
2024-03-30 10:49:02 +0100

    Test Connexion gitHub

--- exo_2_1/main.java
+++ exo_2_1/main.java
@@ -5,19 +5,8 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		int[] tableau=tab(5);
+		System.out.println("Test");
 		
-	}
-	public static int[] tab(int indice) {
-		int[] tab= new int[indice];
-		for(int i=0; i<indice;i++) {
-			System.out.println("Inserer la valeur : "+ i );
-			Scanner scan=new Scanner(System.in);
-			tab[i]=scan.nextInt();
-		}
-		
-		
-		return tab;
 	}
 
 }
